38e01cb73cf5c52e41f8b450207f419570289974
Udith Ishara <isharalou@yahoo.com>
2018-12-04 21:27:07 +0530

    Add React-route to test on GitHub pages

--- src/App.js
+++ src/App.js
@@ -1,28 +1,57 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Link, Route } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Header key="Header" />
+
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route path="/About" component={About}/>
+          <Route component={NoMatch} />
+        </Switch>
+
       </div>
     );
   }
 }
+
+const Header = () => (
+  <header>
+    <nav>
+      <ul className="primary-nav">
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/About'>About</Link></li>
+        <li><Link to='/post/that-does-not-exists'>Post</Link></li>
+      </ul>
+    </nav>
+  </header>
+);
+
+const Home = () => {
+  return [
+    <div key="Home">
+      <h2>Home</h2>
+    </div>
+  ]
+};
+
+const About = () => {
+  return [
+    <div key="About">
+      <h2>About</h2>
+    </div>
+  ]
+};
+
+const NoMatch = ({ location }) => [
+  <div key="Error">
+    <h3>No match for <code>{location.pathname}</code></h3>
+  </div>
+];
+
 
 export default App;
